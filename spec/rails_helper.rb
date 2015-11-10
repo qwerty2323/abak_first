8f060777ad5e867ad16aa967b7b5b937636bf24f
Denis Ilyasov <ilyasovd@hotmail.com>
2015-11-10 16:18:26 +0500

    Добавлены методы в тесты для FactoryGirl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,6 +23,8 @@ require 'rspec/rails'
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   config.use_transactional_fixtures = false
 
   config.before(:suite) do

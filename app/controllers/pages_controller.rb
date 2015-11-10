8ed31cd4479f49e584b4c10a1725924df444dbee
Denis Ilyasov <ilyasovd@hotmail.com>
2015-11-10 16:11:05 +0500

    Добавлен контроллер Pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def new_root
+  end
+
+  def create
+  end
+
+  def create_root
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 AbakFirst::Application.routes.draw do
+  get "pages/index"
+
+  get "pages/show"
+
+  get "pages/new"
+
+  get "pages/new_root"
+
+  get "pages/create"
+
+  get "pages/create_root"
+
+  get "pages/edit"
+
+  get "pages/update"
+
   root to: 'pages#index'
 end

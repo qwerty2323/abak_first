cbb23a7fce4afd0b9a44f5cc202e5e0d173a4a47
Denis Ilyasov <ilyasovd@hotmail.com>
2015-11-10 16:28:48 +0500

    Показ 404 ошибки если страница не найдена

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from NoMethodError, :with => :render_404
+
+  def render_404
+    render("public/404.html")
+  end
 end

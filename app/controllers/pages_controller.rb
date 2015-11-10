c5ff6d096de1a02808643c3092d4a5a58ec771c9
Denis Ilyasov <ilyasovd@hotmail.com>
2015-11-10 16:13:48 +0500

    Добавлены модель и методы контроллера

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,56 @@
 class PagesController < ApplicationController
+  before_filter :set_page, only: [:show, :create, :edit, :update]
+
   def index
+    @pages = Page.roots
   end
 
   def show
+    @pages = @page.children
   end
 
   def new
+    @page = Page.new(parent_id: Page.find_id_by_names(params[:names]))
   end
 
   def new_root
+    @page = Page.new
   end
 
   def create
+    @page = Page.new(params[:page])
+
+    if @page.save
+      redirect_to page_path(@page.names)
+    else
+      render 'new'
+    end
   end
 
   def create_root
+    @page = Page.new(params[:page])
+
+    if @page.save
+      redirect_to page_path(@page.names)
+    else
+      render 'new_root'
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @page.update_attributes(params[:page])
+      redirect_to page_path(@page.names)
+    else
+      render 'edit'
+    end
+  end
+
+private
+
+  def set_page
+    @page = Page.find_by_names(params[:names])
   end
 
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,36 @@
+# encoding: UTF-8
+
 class Page < ActiveRecord::Base
+  has_ancestry
+
+  validates :name, presence: true, uniqueness: true, format: { with: /[a-zA-Zа-яА-Я0-9_]/ }
+  validates :title, presence: true
+
+  before_save :format_body
+
+  def names
+    path.map(&:name).join('/')
+  end
+
+  def self.find_by_names(names)
+    return nil if names.nil?
+    last_name = names.split("/").last
+    find_by_name(last_name)
+  end
+
+  def self.find_id_by_names(names)
+    return nil if names.nil?
+    find_by_names(names).id
+  end
+
+  def format_body
+    return nil if body.nil?
+    bold_regexp = Regexp.new('\*\*(?<text>.+)\*\*')
+    italic_regexp = Regexp.new('\\\\\\\\(?<text>.+)\\\\\\\\')
+    a_regexp = Regexp.new('\(\((?<path>[^\s]+)\s(?<text>.+)\)\)')
+
+    self.formatted_body = body.gsub(bold_regexp, '<b>\k<text></b>').
+                               gsub(italic_regexp, '<i>\k<text></i>').
+                               gsub(a_regexp, '<a href="/\k<path>">\k<text></a>')
+  end
 end

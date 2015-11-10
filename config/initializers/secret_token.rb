574ec1e777bdaaf2c258ffa61bf16d9b1d6a0873
Denis Ilyasov <ilyasovd@hotmail.com>
2015-11-10 15:50:33 +0500

    Начальный коммит

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbakFirst::Application.config.secret_token = 'cc7840ae8f822ddf513923ab7f75b1f7047edb5a5bedd9f442755be951c8b3f7a127f3568fdd53f81e86ab24929154c85bd68c35dfc799b7868687afde22120e'
